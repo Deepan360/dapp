1fa559a86c1411874f2f32b00062d017bd942e75
Deepan360 <deepandeep686@gmail.com>
2024-06-13 10:50:57 +0530

    modified for media query in a mobile view

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173 // you can set this to any port you'd like, just make sure it matches your EXPOSE in Dockerfile
+  }
 })
